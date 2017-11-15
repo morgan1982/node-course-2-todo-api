86935f993228691fa8e15d55c0ca4128c38a0a05
morgan1982 <alexandrisanas@gmail.com>
2017-11-15 23:45:41 +0200

    add get /todos/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
+
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
@@ -27,6 +29,7 @@ app.post('/todos', (req, res) => {
         })
 })
 
+// GET
 app.get('/todos', (req, res) => {
     Todo.find()
         .then((todos) => {
@@ -36,6 +39,24 @@ app.get('/todos', (req, res) => {
         }, (err) => {
             res.status(400).send(err);
         })
+})
+// GET /todos/id
+app.get('/todos/:id', (req, res) => {
+
+    let id = req.params.id
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+    Todo.findById(id).then((todo) => {
+    if (!todo) {
+        return res.status(404).send();
+    }
+        res.send({todo});
+        }).catch((e) => {
+    res.status(400).send();
+    })
+
 })
 
 
